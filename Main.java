2b65c0cfca18e83d89a24e46095192bb6436018a
Mbaimbai Takalani <mbaimbaitakalani410@gmail.com>
2026-04-10 23:31:02 +0200

    Update and rename programiiz.txt to Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,30 @@
 // Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+// Use this editor to write, compile, and run your Java code online
 //util library
 import java.util.Scanner;
 //the class
 public class Main {
-    //main mthod
+    //main method
     public static void main(String[] args) {
         //creating scanner
         Scanner scan = new Scanner (System.in);
         //prompting the user to enter the present value
         System.out.println("enter the present value:");
         double presentValue = scan.nextDouble();
-        //asking the user for interest rate
+        //asking the user for the interest rate
         System.out.println("enter the annual rate in decimal;");
         double annualRate = scan.nextDouble();
-        //prompting the user for the number of times the interest rate iscompounded per year
+        //prompting the user for the number of times the interest rate is compounded per year
         System.out.println("enter the number of times your interest rate is compounded per year:");
         double numberOfTimes = scan.nextDouble();
-        //prompting the user to enter amount of time in years
+        //prompting the user to enter the amount of time in years
         System.out.println("enter amount of time in years:");
         double amountOfYears = scan.nextDouble();
-        //values in the formular
+        //values in the formula
        double P = presentValue;
        double r = annualRate;
        double t = amountOfYears;
        double n = numberOfTimes;
-       //using the formular to calculate
+       //using the formula to calculate
       double A = P * Math.pow(1+r/n, n*t);
        System.out.println("Future value is:"+ A);
